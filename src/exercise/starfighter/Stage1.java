8c693545ca32f2ed2c8d3c61d24f0934f0b556e8
Hannes Rosenkranz <Frodo@dynip-129-217-116-130.wifi.tu-dortmund.de>
2017-06-01 13:36:38 +0200

    small Changes

--- src/exercise/starfighter/Stage1.java
+++ src/exercise/starfighter/Stage1.java
@@ -9,14 +9,14 @@ import exercise.monsters.MonsterSpeedDown;
 public class Stage1 extends SpawnMonsters {
 	
 	public Stage1() {
-		setMaxMonstersAtOnce(10); // was 5
+		setMaxMonstersAtOnce(5); // was 5
 		setMonsterSpawnInterval(40);
 		setSpawnDistanceToPlayer(300, 900);
 		setTotalMonsterCount(50);
 		setMonsterFactory((g) -> new MonsterEasy());
 		
 		//Task 39
-		setMonsterFactory((e) -> new MonsterSpeedDown());
+		//setMonsterFactory((e) -> new MonsterSpeedDown());
 		
 		setReactionAfterAllMonstersAreDead(g -> {
 			g.addScript(new Stage2());
